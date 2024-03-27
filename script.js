3dd54c07a4422c29e52be703d1d1b2995228516f
Charlie <136764960+CharlieFitzgerald@users.noreply.github.com>
2024-03-27 22:38:15 +0000

    add code

--- script.js
+++ script.js
@@ -269,7 +269,7 @@ console.log(Object.getGetOwnPropertyNames(swimming.__proto__));*/
 
 // inheritence and polymorphism
 
-class Warrior {
+/*class Warrior {
     constructor(
         name,
         hp,
@@ -309,4 +309,41 @@ class Bartender {
         this.items = items;
         this.mug = mug;
     }
+}*/
+
+//super class
+class Player {
+    
+    constructor(
+        name,
+        hp,
+        mp,
+        items) {
+            
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.items = items;
+    }
 }
+
+//sub class
+class Warrior extends Player {
+    
+    constructor(
+        name,
+        hp,
+        mp,
+        items,
+        shield) {
+        super(name,hp,mp,items);
+
+        this.shield = shield;
+    }
+}
+
+const player = new Player("Player", 100, 10, []);
+console.log(player);
+
+const warrior = new Warrior("Gengis Khan", 300, 30, ["Sabre"], ["Leather Shield"]);
+console.log(warrior);
